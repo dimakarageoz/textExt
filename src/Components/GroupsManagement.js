b6a81b20570d2f21765a4c4cfe5240cbbe7939da
elinsia <elinsia7@gmail.com>
2017-01-23 16:17:46 +0200

    web-cabinet-test

---
+++ src/Components/GroupsManagement.js
@@ -0,0 +1,18 @@
+/**
+ * Created by student on 1/19/17.
+ */
+import React, {Component} from 'react';
+import GroupAddList from './GroupManagCore';
+
+class GroupsManagement extends Component {
+    render(){
+        return(
+            <div>Hello, this page is under development 'Groups Management'
+            <GroupAddList/>
+            </div>
+        );
+    }
+}
+export default GroupsManagement;
+
+
---
+++ src/Components/GroupsPage.js
@@ -0,0 +1,13 @@
+/**
+ * Created by student on 1/19/17.
+ */
+import React, {Component} from 'react';
+
+class GroupsPage extends Component {
+    render(){
+        return(
+            <div>Hello, this page is under development 'Groups Page'</div>
+        );
+    }
+}
+export default GroupsPage;
